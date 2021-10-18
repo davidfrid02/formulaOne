ac0a09ed25ae69513303457d34c68fd8e57ed45f
davidfrid02 <frid.david@gmail.com>
2021-10-18 22:40:40 +0300

    fixed morgan logging

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 const app = express();
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(cors());
 app.use(compression());
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-morgan(':method :url :status :res[content-length] - :response-time ms');
 app.use(require('./routes'));
 
 module.exports = app;
